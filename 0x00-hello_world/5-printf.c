b660a4f83709188f4e91dabafbc005f778b50247
Jonatan Mazo <55111603+MAZTRO@users.noreply.github.com>
2019-09-18 16:06:01 -0500

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 # include <stdio.h>
-/*
-* hola
-*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
