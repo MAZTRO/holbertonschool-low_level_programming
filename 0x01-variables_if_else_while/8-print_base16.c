bd0ffa77309f9e5513284eae78e5325ac853fdba
MAZTRO <designe.mallet@gmail.com>
2019-09-19 16:53:00 -0500

    Hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int al, AL;
+for (al = '0'; al <= '9'; al++)
+{
+putchar(al);
+}
+for (AL = 'a'; AL <= 'f'; AL++)
+{
+putchar(AL);
+}
+putchar('\n');
+return (0);
+}
