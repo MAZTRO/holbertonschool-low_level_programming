1bd006df2211695d6135a8a6b36bc552f7b8d98f
MAZTRO <designe.mallet@gmail.com>
2019-09-24 13:26:27 -0500

    Lol

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *add - Check the character.
+ *
+ *@A: Returned value
+ *@B: Returned value
+ *Return: Always
+ */
+int add(int A, int B)
+{
+int Res = 0;
+
+	Res = A + B;
+return(Res);
+}
