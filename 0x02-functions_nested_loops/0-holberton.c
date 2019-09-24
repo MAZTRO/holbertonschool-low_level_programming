23bb174fd68bec4dea179abffc6b107152e45733
Jonatan Mazo <55111603+MAZTRO@users.noreply.github.com>
2019-09-24 10:15:02 -0500

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,16 +1,19 @@
 #include "holberton.h"
 
 /**
- * my_function - This is a description
+ *my_function - This is a description
  */
 int main(void)
 {
-int num;
-char Cara[10] = {"Holberton"};
-for (num = 0; num <= 9; num++)
-{
-_putchar(Cara[num]);
-}
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
 _putchar('\n');
 return (0);
 }
