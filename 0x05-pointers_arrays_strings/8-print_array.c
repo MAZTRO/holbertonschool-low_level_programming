9c0e40e1f2a42a2f3123585eec2300d8d22d38fa
MAZTRO <designe.mallet@gmail.com>
2019-10-01 16:09:37 -0500

    lol

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ *print_array - print the string in revers
+ *@a: the variable that contain the string
+ *@n: characters count
+ */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	x = 0;
+
+	while (x < n)
+	{
+		if ((x + 1) == n)
+		{
+			printf("%d", a[x]);
+		}
+		else
+		{
+			printf("%d, ", a[x]);
+		}
+		x++;
+	}
+/*
+ *	for (x = 0; x < n; x++)
+ *	{
+ *		if ((x + 1) == n)
+ *		{
+ *			printf("%d", a[x]);
+ *		}
+ *		else
+ *		{
+ *			printf("%d, ", a[x]);
+ *		}
+ *	}
+ */
+	printf("\n");
+}
