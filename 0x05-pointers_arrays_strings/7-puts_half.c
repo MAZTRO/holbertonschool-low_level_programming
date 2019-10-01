96840defc81fcfe5cb6a646996ee11af80a18ba9
MAZTRO <designe.mallet@gmail.com>
2019-10-01 14:42:40 -0500

    7-puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *puts_half - print the string in revers
+ *@str: the variable that contain the string
+ */
+
+void puts_half(char *str)
+{
+	int t, n;
+
+	for (t = 0; str[t] != '\0'; t++)
+	{
+
+	}
+
+	n = (t - 1) / 2;
+
+	while (t > n)
+	{
+			n++;
+			_putchar(str[n]);
+	}
+
+	_putchar('\n');
+}
