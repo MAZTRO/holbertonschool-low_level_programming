e1658d8cdc9d861197bcd4dcc9c547baefca4362
MAZTRO <designe.mallet@gmail.com>
2019-09-30 21:04:39 -0500

    Lol

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,23 @@
 #include "holberton.h"
 
 /**
- *
+ *print_rev - print the string in revers
+ *@s: the variable that contain the string
  */
 
 void print_rev(char *s)
 {
+	int a;
 
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		/* Nothing */
+	}
+
+	while (a >= 0)
+	{
+		_putchar(s[a]);
+		a--;
+	}
+	_putchar('\n');
 }
