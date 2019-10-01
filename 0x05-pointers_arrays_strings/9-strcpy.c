7e96139314a206a199fb9ea34556bfd2cdc78f2e
MAZTRO <designe.mallet@gmail.com>
2019-10-01 17:56:51 -0500

    lok

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - check the code for Holberton School students.
+ * @dest: lol
+ * @src: lol
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+}
