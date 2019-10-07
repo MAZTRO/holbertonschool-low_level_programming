7ff7fc708136ef73883e2a005d057869aa51ce4f
MAZTRO <designe.mallet@gmail.com>
2019-10-07 16:11:28 -0500

    Lol

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strchr - check the code for Holberton School students.
+ * @s: lol
+ * @c: lol
+ * Return: Always 0.
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] == c)
+		{
+			s += x;
+			return (s);
+		}
+	}
+
+	x += 1;
+	s += x;
+	return (s);
+}
