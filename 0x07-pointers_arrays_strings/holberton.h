510fc932782e4bc9ca340f7a96940059931cb437
Jonatan Mazo <55111603+MAZTRO@users.noreply.github.com>
2019-10-07 22:09:39 -0500

    Update holberton.h

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
