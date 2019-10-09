8263041e17d4002e1dbb902d611fcd487a91ddf9
MAZTRO <designe.mallet@gmail.com>
2019-10-09 18:19:15 -0500

    lsfdkdslf

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - check the code for Holberton School students.
+ * @n: Sqrt
+ * Return: Always 0.
+ */
+
+int _sqrt_recursion(int n)
+{
+	mysqrt(n);
+	
+}
+
+int mysqrt(int y)
+{
+	int x = 1;
+
+	
+}
