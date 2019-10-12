c0dd7260de6d7ee672ca937daec394ae602ba3ba
MAZTRO <designe.mallet@gmail.com>
2019-10-12 17:59:56 -0500

    Arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define UNUSED(argc) (void)(argc)
+
+/**
+ * main - Lol
+ * @argc: lol
+ * @argv: Lol
+ * Return: Lol
+ */
+
+int main(int argc, char *argv[])
+{
+	UNUSED(argc);
+
+		printf("%s\n", *argv);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+### ARGC and ARGV
+Arguments in the shell for a C file
