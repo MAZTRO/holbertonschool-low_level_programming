8be51baec3bf62c276a976f307a70866a3d6c72b
System Administrator <root@Holbertons-iMac-7.local>
2019-10-15 19:02:52 -0500

    Malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,15 +14,23 @@ char *str_concat(char *s1, char *s2)
 	int x, y, size, concat, concat2;
 	char *Arr_concat;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
 	for (x = 0; s1[x] != '\0'; x++)
 	for (y = 0; s2[y] != '\0'; y++)
 
-	size = x + y;
+	size = (x + 1) + (y + 1);
 
-	Arr_concat = malloc(sizeof(char) * (size + 1));
+	Arr_concat = malloc(sizeof(char) * (size));
 
-	if (Arr_concat == NULL || size == 0)
+	if (Arr_concat == NULL)
 	{
 		return (NULL);
 	}
@@ -36,7 +44,7 @@ char *str_concat(char *s1, char *s2)
 		Arr_concat[x + concat2] = s2[concat2];
 	}
 
-	Arr_concat[concat2] = '\0';
+	Arr_concat[concat2 + 1] = '\0';
 
 	return (Arr_concat);
 }
