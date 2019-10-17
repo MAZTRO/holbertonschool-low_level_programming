c2d61ac7449f9944fc3cd2eaba1c56808ad3b94f
System Administrator <root@Holbertons-iMac-7.local>
2019-10-17 15:33:44 -0500

    More Malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - prints buffer in hexa
+ * @nmemb: the address of memory to print
+ * @size: the size of the memory to print
+ * Return: Nothing.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *Arr;
+	unsigned int x;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	Arr = malloc(size * nmemb);
+
+	if (Arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (x = 0; x < nmemb; x++)
+	{
+		Arr[x] = 0;
+	}
+
+	return (Arr);
+}
