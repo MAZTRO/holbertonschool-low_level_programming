5be0ee451fede33e7aa22e8c19a9ba05cb566fe8
System Administrator <root@Holbertons-iMac-7.local>
2019-10-17 19:20:01 -0500

    Malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,15 +8,12 @@ int *array_range(int min, int max)
 	int x, *Arr;
 
 	if (min > max)
-	{
 		return (NULL);
-	}
 	Arr = malloc((max - min + 1) * sizeof(int));
 
 	if (Arr == NULL)
-	{
+
 		return (NULL);
-	}
 
 	for (x = 0; x <= (max - min); x++)
 		Arr[x] = min + x;
