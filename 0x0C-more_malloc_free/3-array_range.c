6db1e5fc31e5291c63b74ef402b50f8db1557d3a
System Administrator <root@Holbertons-iMac-7.local>
2019-10-17 19:22:04 -0500

    Malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * array_range - prints buffer in hexa
+ * @min: the address of memory to print
+ * @max: the size of the memory to print
+ * Return: Nothing.
+ */
+
 int *array_range(int min, int max)
 {
 	int x, *Arr;
