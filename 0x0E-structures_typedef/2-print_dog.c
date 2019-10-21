607aff9bde0e59262da7d40c6408cf1b7efc3f51
MAZTRO <designe.mallet@gmail.com>
2019-10-21 17:41:05 -0500

    TipeDef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (*d).name == NULL ? "(nil)" : (*d).name);
-		printf("Age: %.6f\n", (*d).age);
+		printf("Age: %f\n", (*d).age);
 		printf("Owner: %s\n", (*d).owner == NULL ? "(nil)" : (*d).owner);
 	}
 }
