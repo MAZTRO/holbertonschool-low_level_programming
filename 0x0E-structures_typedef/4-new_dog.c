3d46e7412b3221f52a9dc431cb63ef9dc7050d90
MAZTRO <designe.mallet@gmail.com>
2019-10-21 21:55:47 -0500

    New Dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,26 +1,49 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * new_dog - Lol
- * @name: Lol
- * @age: Lol
- * @owner: Lol
- * Return: Lol
+ * *new_dog - function that gets the length of a prefix substring.
+ * @name: The first string
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: return dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *Name = malloc(sizeof(name));
-	if (Name == NULL)
-	{
+	dog_t *trosky;
+	int i = 0, j = 0;
+
+	name == NULL ? name = "" : name;
+	owner == NULL ? owner = "" : owner;
+
+	trosky = malloc(sizeof(dog_t));
+	if (trosky == NULL)
 		return (NULL);
-	}
-	char *Owner = malloc(sizeof(owner));
-	if (Owner == NULL)
+
+	i = strlen(name);
+	j = strlen(owner);
+
+	(*trosky).name = malloc(i + 1);
+	if ((*trosky).name == NULL)
 	{
+		free(trosky);
 		return (NULL);
 	}
 
+	(*trosky).owner = malloc(j + 1);
+	if ((*trosky).owner == NULL)
+	{
+		free((*trosky).name);
+		free(trosky);
+		return (NULL);
+	}
 
+	strcpy((*trosky).name, name);
+	strcpy((*trosky).owner, owner);
+
+	(*trosky).age = age;
+	return (trosky);
 }
