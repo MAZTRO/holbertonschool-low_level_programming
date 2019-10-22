bda32a44021bdded46133a95fc277c506b9f5573
MAZTRO <designe.mallet@gmail.com>
2019-10-21 22:11:16 -0500

    New dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - Lol
+ * @d: Lol
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+    {
+		return;
+    }
+	else
+	{
+	    free(d->name);
+	    free(d->owner);
+	    free(d);
+	}
+}
