2c4916aa6d6079e4ec92a56e3990b73d38d14a71
System Administrator <root@Holbertons-iMac-78.local>
2019-10-23 13:59:40 -0500

    Betty

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name as is
  * @name: name of the person
- *
+ * @f: Pointer of function
  * Return: Nothing.
  */
 
