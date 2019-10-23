5b07e01c6ff3c94b3a42df3a66b9ef3a418f6196
System Administrator <root@Holbertons-iMac-78.local>
2019-10-23 13:21:46 -0500

    Pointer to functions

---
+++ 0x0F-function_pointers0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer.
+ * @array: Array.
+ * @size: Length of array.
+ * @cmp: the pointer.
+ * Return: something else otherwise.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x, compare;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			if (is_98() == )
+			{
+				return (x);
+			}
+		}
+	}
+	else
+	{
+		return (-1);
+	}
+
+}
