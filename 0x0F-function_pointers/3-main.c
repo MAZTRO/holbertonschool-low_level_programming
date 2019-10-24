1c1f569909d9917010854717fd69f527c1f9fc2d
System Administrator <root@Holbertons-iMac-78.local>
2019-10-23 20:10:11 -0500

    Calc - incomplet MAIN

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - Get the operators
+ * @s: the operator
+ * Return: the function of operators
+ */
+
+int main(int argc, char *argv[])
+{
+	if (atoi(argv) != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((get_op_func == '/' || get_op_func == '%') && argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", ;
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Sum Operations
+ * @a: First number
+ * @b: Second number
+ * Return: The result of operation
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Substrac Operations
+ * @a: First number
+ * @b: Second number
+ * Return: The result of operation
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplicate Operations
+ * @a: First number
+ * @b: Second number
+ * Return: The result of operation
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div Operations
+ * @a: First number
+ * @b: Second number
+ * Return: The result of operation
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod Operations
+ * @a: First number
+ * @b: Second number
+ * Return: The result of operation
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
