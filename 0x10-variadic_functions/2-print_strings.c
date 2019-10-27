12e00a308ccdfb22f6194055158e3eb2ec57195a
MAZTRO <designe.mallet@gmail.com>
2019-10-26 20:04:34 -0500

    Lol

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,16 +27,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (prints == NULL)
 		{
-			prints = "(nil)";
+			printf("(nil)");
 		}
+
 		printf("%s", prints);
 
 		if (separator != NULL && (x + 1) != n)
 		{
 			printf("%s", separator);
 		}
-		x += 1;
+		x++;
 	}
 	va_end(myprint);
-	printf("\n");
+	putchar('\n');
 }
