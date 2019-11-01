2d93f1326c49a6a075f703c061b6ca6a23ee10ee
MAZTRO <designe.mallet@gmail.com>
2019-10-31 19:24:39 -0500

    First 3 task

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * *add_node - writes the character c to stdout
+ * @head: The character to print
+ * @str: name
+ *
+ * Return: list
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *cpy;
+	int i;
+
+	cpy = malloc(sizeof(list_t));
+	if (cpy == NULL)
+	{
+		return (NULL);
+	}
+	cpy->str = strdup(str);
+
+	for (i = 0; str[i] != '\0'; i++);
+	
+	cpy->len = i;
+	cpy->next = *head;
+	*head = cpy;
+
+	return (cpy);
+}
