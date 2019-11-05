3bab860a7d4b2f2a0295216c0d91688299c54ff1
MAZTRO <designe.mallet@gmail.com>
2019-11-05 15:24:24 -0500

    lol

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		else
 		{
-			while (prev_node != NULL)
+			while (prev_node != NULL && idx >= count)
 			{
 				if ((count + 1) == idx)
 				{
