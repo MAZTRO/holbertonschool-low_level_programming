45edf3685278331b2cbdb49c233f8f53ed72a9f3
MAZTRO <designe.mallet@gmail.com>
2019-11-05 12:27:03 -0500

    lol

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *next_node;
 	unsigned int count = 0;
 
-	if (*head != NULL && idx >= 0)
+	if (*head != NULL)
 	{
 		new_node = malloc(sizeof(listint_t));
 		if (new_node == NULL)
