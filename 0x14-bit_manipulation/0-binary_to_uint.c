7c229ea151ca4700c82b456b6ef86df9074050e6
MAZTRO <designe.mallet@gmail.com>
2019-11-07 16:28:17 -0500

    Libraries

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
  * my_pow - check the code for Holberton School students.
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,13 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <unistd.h>
-#include <math.h>
-
-int _pow(int x, int y);
+int _putchar(char c);
+unsigned int my_pow(int x, int y);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 
