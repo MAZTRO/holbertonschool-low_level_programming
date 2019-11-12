c470466f911e16e82df76c20db9ec579af43f578
MAZTRO <designe.mallet@gmail.com>
2019-11-12 14:05:55 -0500

    first task

---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+## Standar inputs and Outputs
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <sys/types.h>
+#include <sys/uio.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
