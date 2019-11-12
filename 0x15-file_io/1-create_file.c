27aba90dc2e3b46ded6e1405c9ded1e63ff6e742
MAZTRO <designe.mallet@gmail.com>
2019-11-12 15:24:45 -0500

    Fix empty text

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,31 +15,25 @@ int create_file(const char *filename, char *text_content)
 {
 	int ID = 0, wr = 0, count;
 
-	for (count = 0; text_content[count] != '\0'; count++)
-	{}
-
-	if (text_content == NULL)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		text_content = "";
+		for (count = 0; text_content[count] != '\0'; count++)
+		{}
 	}
-	if (filename != NULL)
+	ID = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (ID == -1)
 	{
-		ID = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-		if (ID == -1)
-		{
-			return (-1);
-		}
-		wr = write(ID, text_content, count);
-		if (wr == -1)
-		{
-			return (-1);
-		}
-		close(ID);
-		return (1);
+		return (-1);
 	}
-	return (-1);
+	wr = write(ID, text_content, count);
+	if (wr == -1)
+	{
+		return (-1);
+	}
+	close(ID);
+	return (1);
 }
