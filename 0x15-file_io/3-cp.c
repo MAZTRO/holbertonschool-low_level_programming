17f6924a48fe1c22ffa2d823bdeec30bd2e3d6d5
MAZTRO <designe.mallet@gmail.com>
2019-11-12 19:53:25 -0500

    Fix Betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,31 +29,31 @@ int main(int argc, char *argv[])
 	file_origin = open(argv[1], O_RDONLY);
 	if (file_origin == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	file_dest = open(argv[2], O_CREAT | O_WRONLY, 0664);
 	if (file_dest == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	while ((numb = read(file_origin, buff, BUFF_SIZE)) > 0)
 	{
 		if (write(file_dest, buff, numb) != numb)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 	if (numb == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if ((close(file_origin) == -1) || (close(file_dest) == -1))
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_origin);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_origin);
 		exit(100);
 	}
 	exit(EXIT_SUCCESS);
