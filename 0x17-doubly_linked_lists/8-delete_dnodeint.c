74bf83af5e898aaf7c2debf13ced85b7a1231225
MAZTRO <designe.mallet@gmail.com>
2019-12-14 12:58:43 -0500

    Delete a Node

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,92 @@
+#include "lists.h"
+
+/**
+ * get_node_at_index - get the node expected listint_t list
+ * @head: pointer to head of list
+ * @index: Place of a node to return
+ * Return: The node.
+ */
+
+dlistint_t *get_node_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp, *current;
+	size_t count = 0, len = 0;
+
+	temp = head;
+	current = head;
+	if (head != NULL && head->prev == NULL)
+	{
+		while (current != NULL)
+		{
+			current = current->next;
+			len++;
+		}
+	}
+	if (head != NULL)
+	{
+
+		while (temp->next != NULL && len >= index)
+		{
+			if (index == count)
+			{
+				return (temp);
+			}
+			temp = temp->next;
+			count++;
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * delete_dnodeint_at_index - delete the node expected listint_t list
+ * @head: pointer to head of list
+ * @index: Place of a node to return
+ * Return: The node.
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp, *current;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+	{return (-1); }
+	temp = *head;
+	current = *head;
+	while (temp->next != NULL)
+	{temp = temp->next, count++; }
+	if (index == 0 && count > 0)
+	{
+		current = current->next;
+		current->prev = NULL;
+		(*head)->next = NULL;
+		free(*head);
+		*head = current;
+	}
+	else if (count == index)
+	{
+		if ((**head).next == NULL && (**head).prev == NULL)
+		{
+			free(*head);
+			*head = NULL;
+			return (1);
+		}
+		current = temp;
+		current = current->prev;
+		current->next = NULL;
+		temp->prev = NULL;
+		free(temp);
+		temp = current;
+	}
+	else if (index < count)
+	{
+		current = get_node_at_index(*head, index);
+		temp = current->prev;
+		current = current->next;
+		free(temp->next);
+		temp->next = current;
+		current->prev = temp;
+	}
+	return (1);
+}
