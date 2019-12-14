e6f44089c90ade985fa6cef9a09f8904bf2e98e9
MAZTRO <designe.mallet@gmail.com>
2019-12-14 16:22:00 -0500

    Fix issues on 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -42,12 +42,10 @@ dlistint_t *get_node_at_index(dlistint_t *head, unsigned int index)
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current, *new = NULL, *temp;
+	dlistint_t *new = NULL, *temp = NULL;
 	unsigned int count = 0;
 
 	temp = *h;
-	while (temp->next != NULL)
-	{temp = temp->next, count++; }
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
@@ -56,18 +54,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint_end(h, n));
 	}
-	else if (idx < count)
+	else
 	{
 		new = malloc(sizeof(dlistint_t));
 		if (new == NULL)
 		{return (NULL); }
-		current = get_node_at_index(*h, idx);
-		new->n = n;
-		new->next = current;
-		new->prev = current->prev;
-		current->prev->next = new;
-		current->prev = new;
-		return (new);
+		while (temp != NULL)
+		{
+			if (idx == count)
+			{
+				new->n = n;
+				new->next = temp;
+				new->prev = temp->prev;
+				temp->prev->next = new;
+				temp->prev = new;
+				return (new);
+			}
+			temp = temp->next;
+			count++;
+		}
 	}
 	return (NULL);
 }
