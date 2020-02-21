8556bf24919d3af04ae92f95e194dc874a2560ef
MAZTRO <design.mallet@gmail.com>
2020-02-21 22:09:27 +0000

    Fix pep8 of Advanced task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,26 +4,31 @@
 
 def island_perimeter(grid):
     """ Return the add of all positions found """
-    """ the lines commented with #, was used to illustrate the functionality """
+    """
+    The lines commented with #,
+    was used to illustrate the functionality
+    """
     # print("============\n")
     lis = []
     per = 0
-    if len(grid) < 100:
-        for x in range(len(grid)):
-            if len(grid[x]) < 100:
-                for y in range(len(grid[x])):
-                    if (grid[x][y] == 1):
+    g = grid
+    if len(g) < 100:
+        for x in range(len(g)):
+            if len(g[x]) < 100:
+                for y in range(len(g[x])):
+                    if (g[x][y] == 1):
                         per = 4
-                        if (grid[x - 1][y] == 1 and x > 0):
+                        if (g[x - 1][y] == 1 and x > 0):
                             per -= 1
-                        if ((x + 1) <= len(grid) - 1 and grid[x + 1][y] == 1):
+                        if ((x + 1) <= len(g) - 1 and g[x + 1][y] == 1):
                             per -= 1
-                        if ((y + 1) <= len(grid[x]) - 1 and grid[x][y + 1] == 1):
+                        if ((y + 1) <= len(g[x]) - 1 and g[x][y + 1] == 1):
                             per -= 1
-                        if (grid[x][y - 1] == 1 and y > 0):
+                        if (g[x][y - 1] == 1 and y > 0):
                             per -= 1
                         lis.append(per)
-            # print(grid[x], " -- ", "#: {}".format(x), "Grid: {}".format(len(grid)), "Grid[x]: {}".format(len(grid[x])))
+            # print(g[x], " -- ", "#: {}".format(x),
+            # "Grid: {}".format(len(g)), "Grid[x]: {}".format(len(g[x])))
     # print("\n============\n")
     # print(lis)
     return (sum(lis))
